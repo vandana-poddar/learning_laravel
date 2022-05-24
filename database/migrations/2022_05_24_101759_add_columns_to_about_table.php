bc4e2c6173952d919765000c2a202a7584878029
Vandana Poddar <vandanapoddar0609@gmail.com>
2022-05-24 16:00:00 +0530

    added_tables

---
+++ database/migrations/2022_05_24_101759_add_columns_to_about_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('about', function (Blueprint $table) {
+            $table->text('address');
+            $table->string('city',50)->nullable();
+            $table->string('state',10);
+            $table->integer('pincode');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('about', function (Blueprint $table) {
+        });
+    }
+};
